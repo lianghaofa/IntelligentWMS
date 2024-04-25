de041b0b0f39025f1b9e3f42de8a17fdb79efaa9
lianghaofa <1016907063@qq.com>
2024-04-25 20:59:30 +0800

    feat(-1):添加公司模块以及模块模型。

---
+++ company/src/main/java/org/iwms/model/Company.java
@@ -0,0 +1,31 @@
+package org.iwms.model;
+
+import java.util.Date;
+
+/**
+ * @author leung
+ */
+public class Company {
+
+    // 公司名称
+    private String companyName;
+    // 公司所在城市
+    private String companyCity;
+    // 公司地址
+    private String companyAddress;
+    // 公司联系方式
+    private String companyContact;
+    // 公司经理
+    private String companyManager;
+    // 创建者
+    private String creater;
+    // 用户ID
+    private String openid;
+    // 是否删除标签
+    private boolean isDelete;
+    // 创建时间
+    private Date createTime;
+    // 更新时间
+    private Date updateTime;
+
+}
