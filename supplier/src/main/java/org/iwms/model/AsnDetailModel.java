f5f3e1be2e48283c031c908a3eca19b3f8ccc2d7
lianghaofa <1016907063@qq.com>
2024-04-25 20:50:52 +0800

    feat(-1):添加供应商模块模型。

---
+++ supplier/src/main/java/org/iwms/model/AsnDetailModel.java
@@ -0,0 +1,49 @@
+package org.iwms.model;
+
+import java.util.Date;
+
+/**
+ * @author leung
+ */
+public class AsnDetailModel {
+    // ASN编号
+    private String asnCode;
+    // ASN状态
+    private long asnStatus;
+    // 供应商
+    private String supplier;
+    // 商品编码
+    private String goodsCode;
+    // 商品描述
+    private String goodsDesc;
+    // 商品数量
+    private long goodsQty;
+    // 实际商品数量
+    private long goodsActualQty;
+    // 分拣数量
+    private long sortedQty;
+    // 商品短缺数量
+    private long goodsShortageQty;
+    // 商品超量数量
+    private long goodsMoreQty;
+    // 商品损坏数量
+    private long goodsDamageQty;
+    // 商品重量
+    private double goodsWeight;
+    // 商品体积
+    private double goodsVolume;
+    // 商品成本
+    private double goodsCost;
+    // 创建者
+    private String creater;
+    // 用户ID
+    private String openid;
+    // 是否删除标签
+    private boolean isDelete;
+    // 创建时间
+    private Date createTime;
+    // 更新时间
+    private Date updateTime;
+
+    // Getters and setters
+}
---
+++ supplier/src/main/java/org/iwms/model/AsnListModel.java
@@ -0,0 +1,38 @@
+package org.iwms.model;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * @author leung
+ */
+public class AsnListModel {
+    // ASN编号
+    private String asnCode;
+    // ASN状态
+    private long asnStatus;
+    // 总重量
+    private double totalWeight;
+    // 总体积
+    private double totalVolume;
+    // 总成本
+    private double totalCost;
+    // 供应商
+    private String supplier;
+    // 创建者
+    private String creater;
+    // 条形码
+    private String barCode;
+    // 用户ID
+    private String openid;
+    // 运输费用
+    private Map<String, Object> transportationFee;
+    // 是否删除标签
+    private boolean isDelete;
+    // 创建时间
+    private Date createTime;
+    // 更新时间
+    private Date updateTime;
+
+    // Getters and setters
+}
