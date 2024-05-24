399809ec5933a51b5e3beb27c20650f6e09661c2
lianghaofa <1016907063@qq.com>
2024-05-24 23:39:40 +0800

    feat(-1):库存模块添加服务注册客户端。

--- stock/src/main/java/org/iwms/stock/StockApplication.java
+++ stock/src/main/java/org/iwms/stock/StockApplication.java
@@ -3,12 +3,14 @@ package org.iwms.stock;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author leung
  */
 @SpringBootApplication
 @MapperScan("org.iwms.stock.mapper")
+@EnableDiscoveryClient
 public class StockApplication {
 
     public static void main(String[] args) {
